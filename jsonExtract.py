148b3404e846fd02433b0599d10f0bce044b7c26
shaunakganorkar <shaunakganorkar@users.noreply.github.com>
2017-03-16 07:04:38 +0000

    Updated Parser Script
    The new script reduces the unnecessary characters and strings such as u'name: u' & u'address : u'

--- jsonExtract.py
+++ jsonExtract.py
@@ -1,7 +1,7 @@
 #python script for extracting JSON strings from cuckoo sandbox output
 #An object with string containing multiple string:value pairs (nested string:value pairs) <== This is our struggle
 
-
+import os
 import json
 from pprint import pprint
 with open('/home/shaunak/Desktop/Scripts/report.json') as json_file:
@@ -15,20 +15,6 @@ virustotal_sha256 = (str(data['virustotal']['sha256']))
 
 static_pdb_path = (str(data['static']['pdb_path']))
 
-#strings = (str(data['strings']))
-
-#pprint (str(data['static']['pe_imports']['imports'][4]))
-#pprint (str(data['behavior']['summary']))
-#pprint (str(data['strings']))
-#print info_monitor
-#print info_machine_name
-#print virustotal_normalized
-#behavior_generic = (str(data['behavior']['generic'][0]))
-
-
-
-
-
 write_in_file = open('resultFile.txt','w')
 write_in_file.write(virustotal_sha256 +'\n'+ info_machine_name + '\n' + info_monitor + '\n')
 write_in_file.write(static_pdb_path + '\n')
@@ -39,9 +25,30 @@ for item in to_dict['behavior']['generic']:
     
     write_in_file.write(behavior_generic)
 
-    #print behavior_generic                                       #<====== I need to write this on a file. However it says TypeError: expected a string or other character buffer object
-
-
 #write_in_file.write('\n'+'\n'+ strings + '\n')
 
+to_dict = data
+for item in to_dict['static']['pe_imports']:
+    static_pe_imports = str(item['imports'])
+    
+    write_in_file.write('\n'+static_pe_imports+'\n')
 write_in_file.close()
+
+
+with open('resultFile.txt', 'r') as file :
+  filedata = file.read()
+
+# Replace the target string
+filedata = filedata.replace("u'name': u'", '')
+filedata = filedata.replace("u'address': u'", '')
+filedata = filedata.replace('{', '')
+filedata = filedata.replace('}', '')
+filedata = filedata.replace('[', '')
+filedata = filedata.replace(']', '')
+filedata = filedata.replace("'", '')
+
+# Write the file out again
+with open('filteredResult.txt', 'w') as file:
+  file.write(filedata)
+
+os.remove("/home/shaunak/Desktop/Scripts/resultFile.txt")
